0d9565fbaa890968df921f1a7d28411466881290
Markus Mayer <widemeadows@gmail.com>
2024-06-23 20:35:17 +0200

    Add estimation of data rates
    Signed-off-by: Markus Mayer <widemeadows@gmail.com>

--- examples/simulation.rs
+++ examples/simulation.rs
@@ -39,14 +39,51 @@ struct HMC5833L {
     compass_z: f32,
 }
 
+pub trait Time {
+    /// Gets the sample time.
+    fn time(&self) -> f32;
+}
+
+impl Time for MPU6050 {
+    fn time(&self) -> f32 {
+        self.time
+    }
+}
+
+impl Time for HMC5833L {
+    fn time(&self) -> f32 {
+        self.time
+    }
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
     let mpu6050 =
         read_csv::<MPU6050>("tests/data/sensor-fusion/data/set-2/full-sphere/mpu6050.csv")?;
     let hmc5833l =
         read_csv::<HMC5833L>("tests/data/sensor-fusion/data/set-2/full-sphere/hmc5833l.csv")?;
-    println!("{:#?}", mpu6050);
-    println!("{:#?}", hmc5833l);
+
+    // println!("{:#?}", mpu6050[0]);
+    // println!("{:#?}", hmc5833l[0]);
+
+    // Determine sample rates.
+    let (mpu6050_sample_rate, _) = determine_sampling(&mpu6050);
+    let (hmc8533l_sample_rate, _) = determine_sampling(&hmc5833l);
+
+    println!("Average sample rates:");
+    println!("- MPU6050 readings:  {mpu6050_sample_rate} Hz (expected 100 Hz)");
+    println!("- HMC8533L readings: {hmc8533l_sample_rate} Hz (expected 75 Hz)");
+
     Ok(())
+}
+
+fn determine_sampling<T: Time>(data: &[T]) -> (f32, f32) {
+    let (total_diff, count) = data.windows(2).fold((0.0, 0), |(sum, cnt), window| {
+        let diff = window[1].time() - window[0].time();
+        (sum + diff, cnt + 1)
+    });
+    let sample_time = total_diff / (count as f32);
+    let sample_rate = 1.0 / sample_time;
+    (sample_rate, sample_time)
 }
 
 fn read_csv<T: DeserializeOwned>(file_path: &str) -> Result<Vec<T>, Box<dyn Error>> {
